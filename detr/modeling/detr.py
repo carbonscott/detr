44a588fd6a1477f02a73ecaf5a59d20d6ee31cd1
carbonscott <wangimagine@gmail.com>
2023-10-11 12:01:41 -0700

    Bookkeep a first version of detr.

--- detr/modeling/detr.py
+++ detr/modeling/detr.py
@@ -14,159 +14,109 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from .image_encoder import ImageEncoder
+from .transformer   import TransformerEncoderDecoder
 
 from ..configurator import Configurator
 
 
-class MultiHeadAttention(nn.Module):
-    """
-    DETR specific Multi-headed attention.
-    """
-    def __init__(self, embd_size,
-                       context_length,
-                       head_size,
-                       uses_causal_mask  = False,
-                       attention_dropout = 0.0,
-                       residual_dropout  = 0.0):
-        super().__init__()
-
-        self.embd_size         = embd_size
-        self.head_size         = head_size
-        self.attention_dropout = attention_dropout
-        self.residual_dropout  = residual_dropout
-        self.uses_causal_mask  = uses_causal_mask
-
-        # Internal variable
-        self._num_heads = embd_size // head_size
-
-        # Self-attention layer to update each node by aggregating features from all other nodes...
-        # The message-passing based communication happens in another vector space.
-        self.proj_q = nn.Linear(self.embd_size, self.embd_size)    # What do I (this node) want?
-        self.proj_k = nn.Linear(self.embd_size, self.embd_size)    # What do I have publicly?
-        self.proj_v = nn.Linear(self.embd_size, self.embd_size)    # What do I provide to update the entire graph?
-
-        # Store a mask to prevent it from gradient tracking...
-        mask = torch.ones(context_length, context_length).triu(diagonal=1).bool()
-        self.register_buffer('mask', mask)
-
-        # Linear projection...
-        self.proj_linear = nn.Linear(embd_size, embd_size)
-
-        # Use dropout after softmax...
-        self.attention_dropout = nn.Dropout(attention_dropout)
-
-        # Use dropout at the end...
-        self.residual_dropout = nn.Dropout(residual_dropout)
-
-
-    def forward(self, x, x_lookup_k = None, x_lookup_v):
-        B, T, E = x.shape
-
-        num_heads = self._num_heads
-        head_size = self.head_size
-
-        # Linearly project them to a vector space...
-        q = self.proj_q(x)   # B, T, E
-        k = self.proj_k(x if x_lookup_k is None else x_lookup_k)   # B, T, E
-        v = self.proj_v(x if x_lookup_v is None else x_lookup_v)   # B, T, E
-
-        # Changes the view to facilitate scaled dot product within each head...
-        q = q.view(B, T, num_heads, head_size).transpose(1, 2)   # (B, num_heads, T, head_size)
-        k = k.view(B, T, num_heads, head_size).transpose(1, 2)   # (B, num_heads, T, head_size)
-        v = v.view(B, T, num_heads, head_size).transpose(1, 2)   # (B, num_heads, T, head_size)
-
-        # Scaled dot product...
-        w = q @ k.transpose(-1, -2)    # (B, num_heads, T, head_size) @ (B, num_heads, head_size, T) ->
-                                       # (B, num_heads, T, T)
-        w /= torch.sqrt(torch.tensor(head_size))
-
-        # Use causal mask???
-        if self.uses_causal_mask:
-            # Masking in the decoder to enable causal relation...
-            w[:, :,self.mask[:T,:T]] = float('-inf')    # (B, num_heads, :T, :T)   `:T` means upto `T`
-
-        # Obtain the softmax...
-        w = w.softmax(dim = -1)    # (B, num_heads, T, T)
-
-        # Aggregate information from all nodes...
-        a = w @ v    # (B, num_heads, T, T) @ (B, num_heads, T, head_size) ->
-                     # (B, num_heads, T, head_size)
-
-        a = self.attention_dropout(a)
-
-        # Reshape it to (B, T, E)...
-        a = a.transpose(2, 1).contiguous()    # (B, num_heads, T, head_size) -> (B, T, num_heads, head_size)
-        a = a.view(B, T, E)
-
-        # Linear projection...
-        y = self.proj_linear(a)
-
-        # Optional dropout...
-        y = self.residual_dropout(y)
-
-        return y
-
-
-
-
-class Transcoder(nn.Module):
+class DETR(nn.Module):
 
     @staticmethod
     def get_default_config():
-        config = Configurator()
-        with config.enable_auto_create():
-            # Encoder
-            config.encoder.tok_size            = None
-            config.encoder.embd_size           = None
-            config.encoder.context_length      = None
-            config.encoder.num_blocks          = None
-            config.encoder.num_heads           = None
-            config.encoder.uses_causal_mask    = None
-            config.encoder.attention_dropout   = None
-            config.encoder.residual_dropout    = None
-            config.encoder.feedforward_dropout = None
+        CONFIG = Configurator()
+        with CONFIG.enable_auto_create():
+            CONFIG.BACKBONE = ImageEncoder.get_default_config()
+            CONFIG.BACKBONE.OUTPUT_CHANNELS = {
+                'layer4' : 2048,
+            }
+            CONFIG.BACKBONE.OUTPUT_SHAPE = (60, 60)
 
-            # Decoder
-            config.decoder.tok_size            = None
-            config.decoder.embd_size           = None
-            config.decoder.context_length      = None
-            config.decoder.num_blocks          = None
-            config.decoder.num_heads           = None
-            config.decoder.uses_causal_mask    = None
-            config.decoder.attention_dropout   = None
-            config.decoder.residual_dropout    = None
-            config.decoder.feedforward_dropout = None
+            CONFIG.TRANSFORMER.NUM_OBJECT_QUERY   = 100
+            CONFIG.TRANSFORMER.EMBD_SIZE          = 256
+            CONFIG.TRANSFORMER.NUM_BLOCKS         = 6
+            CONFIG.TRANSFORMER.NUM_HEADS          = 8
+            CONFIG.TRANSFORMER.ATTENTION_DROPOUT  = 0.0
+            CONFIG.TRANSFORMER.RESIDUAL_DROPOUT   = 0.0
+            CONFIG.TRANSFORMER.FEEDFOWARD_DROPOUT = 0.0
 
-        return config
+            CONFIG.NUM_CLASSES = 2
+
+        return CONFIG
 
 
-    def __init__(self, config):
+    def __init__(self, config = None):
         super().__init__()
 
-        self.encoder = Transformer(tok_size            = config.encoder.tok_size,
-                                   embd_size           = config.encoder.embd_size,
-                                   context_length      = config.encoder.context_length,
-                                   num_blocks          = config.encoder.num_blocks,
-                                   num_heads           = config.encoder.num_heads,
-                                   uses_causal_mask    = config.encoder.uses_causal_mask,
-                                   attention_dropout   = config.encoder.attention_dropout,
-                                   residual_dropout    = config.encoder.residual_dropout,
-                                   feedforward_dropout = config.encoder.feedforward_dropout
+        self.config = DETR.get_default_config() if config is None else config
 
-        self.decoder = Transformer(tok_size            = config.decoder.tok_size,
-                                   embd_size           = config.decoder.embd_size,
-                                   context_length      = config.decoder.context_length,
-                                   num_blocks          = config.decoder.num_blocks,
-                                   num_heads           = config.decoder.num_heads,
-                                   uses_causal_mask    = config.decoder.uses_causal_mask,
-                                   attention_dropout   = config.decoder.attention_dropout,
-                                   residual_dropout    = config.decoder.residual_dropout,
-                                   feedforward_dropout = config.decoder.feedforward_dropout
+        # Create the image encoder...
+        self.backbone = ImageEncoder(config = self.config.BACKBONE)
+
+        # Transition layer...
+        self.backbone_to_input = nn.Conv2d(in_channels  = self.config.BACKBONE.OUTPUT_CHANNELS['layer4'],
+                                           out_channels = self.config.TRANSFORMER.EMBD_SIZE,
+                                           kernel_size  = 1,
+                                           stride       = 1,
+                                           padding      = 0)
+
+        # Create the learnable positional embedding...
+        H, W = self.config.BACKBONE.OUTPUT_SHAPE
+        self.pos_embd = nn.Parameter(torch.randn(1, self.config.TRANSFORMER.EMBD_SIZE, H, W))
+
+        # Create the transformer module...
+        context_length         = self.config.TRANSFORMER.NUM_OBJECT_QUERY
+        memory_length          = H * W    # ...Spatial memory
+        embd_size              = self.config.TRANSFORMER.EMBD_SIZE
+        num_blocks             = self.config.TRANSFORMER.NUM_BLOCKS
+        num_heads              = self.config.TRANSFORMER.NUM_HEADS
+        attention_dropout      = self.config.TRANSFORMER.ATTENTION_DROPOUT
+        residual_dropout       = self.config.TRANSFORMER.RESIDUAL_DROPOUT
+        feedforward_dropout    = self.config.TRANSFORMER.FEEDFOWARD_DROPOUT
+        self.transformer_endec = TransformerEncoderDecoder(
+                                     context_length      = context_length,
+                                     memory_length       = memory_length,
+                                     embd_size           = embd_size,
+                                     num_blocks          = num_blocks,
+                                     num_heads           = num_heads,
+                                     uses_causal_mask    = False,
+                                     attention_dropout   = 0.0,
+                                     residual_dropout    = 0.0,
+                                     feedforward_dropout = 0.0,
+                                 )
+
+        # Create a shared prediction head for all object queries...
+        num_classes     = self.config.NUM_CLASSES
+        dim_box_repr    = 4    # ...Dimension of bounding box representation
+        self.pred_heads = nn.Linear(embd_size, num_classes + dim_box_repr)
 
 
     def forward(self, x):
         """
-        Cross attention between x and x_lookup.
+
+        Arguments:
+
+            x : torch.Tensor, (B, C, H, W)
+
+        Returns:
+
+            y_hat : torch.Tensor, (B, num_classes + dim_box_repr)
+
         """
-        # Encode input x...
-        x_lookup = self.encoder(x)
+        # Extract features through the backbone network...
+        x = self.backbone(x)
+        x = self.backbone_to_input(x)
+
+        # Attach positional embedding...
+        x = x + self.pos_embd
+
+        # Flatten the embedding...
+        B, C, H, W = x.shape
+        x = x.view(B, C, H * W).transpose(-1, -2).contiguous()
+
+        # Encode and decode through transformer endec...
+        x = self.transformer_endec(x)
+
+        # Make predictions...
+        x = self.pred_heads(x)
+
+        return x
