ecd7ae4dc9a0a9efad5a61547ec6ec93f29617b3
carbonscott <wangimagine@gmail.com>
2023-10-13 10:49:16 -0700

    Give seemingly better the function names.  Add the calculation of areas with tests.

--- detr/box.py
+++ detr/box.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class GIoU(nn.Module):
+class GIOU(nn.Module):
     @staticmethod
     def yxyx_to_yxhw(yxyx):
         y_min, x_min, y_max, x_max = yxyx
@@ -33,7 +33,7 @@ class GIoU(nn.Module):
 
 
     @staticmethod
-    def find_pairwise_superbox(source_boxes, target_boxes):
+    def get_pairwise_superbox_coordinates(source_boxes, target_boxes):
         """
         Arguments:
             source_boxes: [(y_min, x_min, y_max, x_max), ...], shape of (Bs, 4)
@@ -51,7 +51,29 @@ class GIoU(nn.Module):
 
 
     @staticmethod
-    def find_pairwise_intersection(source_boxes, target_boxes):
+    def calculate_pairwise_superbox_area(source_boxes, target_boxes):
+        """
+        Arguments:
+            source_boxes: [(y_min, x_min, y_max, x_max), ...], shape of (Bs, 4)
+            target_boxes: [(y_min, x_min, y_max, x_max), ...], shape of (Bt, 4)
+
+        Returns:
+            area: shape of (Bs, Bt, 4)
+        """
+        super_boxes = GIOU.get_pairwise_superbox_coordinates(source_boxes, target_boxes)
+
+        y_min, x_min, y_max, x_max = super_boxes.permute(2, 0, 1)    # (Bs, Bt, 4) -> (4, Bs, Bt)
+
+        h = y_max - y_min
+        w = x_max - x_min
+
+        area = h * w
+
+        return area
+
+
+    @staticmethod
+    def get_pairwise_intersection_coordinates(source_boxes, target_boxes):
         """
         Arguments:
             source_boxes: [(y_min, x_min, y_max, x_max), ...]
--- tests/test_giou_utils.py
+++ tests/test_giou_utils.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn            as nn
 import torch.nn.functional as F
 
-from detr.box import GIoU
+from detr.box import GIOU
 
 def test_superbox():
     Bs, Bt, N = 10, 12, 4
@@ -21,7 +21,7 @@ def test_superbox():
                                                              super_x_max)],  dim = -1)
 
     # Calculate by the package...
-    super_boxes = GIoU.find_pairwise_superbox(source_boxes, target_boxes)
+    super_boxes = GIOU.get_pairwise_superbox_coordinates(source_boxes, target_boxes)
 
     assert torch.equal(super_boxes, super_boxes_manual)
 
@@ -43,6 +43,29 @@ def test_intersection():
                                                                      intersection_x_max)],  dim = -1)
 
     # Calculate by the package...
-    intersection_boxes = GIoU.find_pairwise_intersection(source_boxes, target_boxes)
+    intersection_boxes = GIOU.get_pairwise_intersection_coordinates(source_boxes, target_boxes)
 
     assert torch.equal(intersection_boxes, intersection_boxes_manual)
+
+
+def test_superbox_area():
+    Bs, Bt, N = 10, 12, 4
+
+    source_boxes = torch.randn(Bs, N)
+    target_boxes = torch.randn(Bt, N)
+
+    # Calculate manually...
+    super_y_min = torch.min(source_boxes[:, None, 0], target_boxes[None, :, 0])
+    super_x_min = torch.min(source_boxes[:, None, 1], target_boxes[None, :, 1])
+    super_y_max = torch.max(source_boxes[:, None, 2], target_boxes[None, :, 2])
+    super_x_max = torch.max(source_boxes[:, None, 3], target_boxes[None, :, 3])
+
+    h_manual = super_y_max - super_y_min
+    w_manual = super_x_max - super_x_min
+    area_manual = h_manual * w_manual
+
+    # Calculate by the package...
+    area = GIOU.calculate_pairwise_superbox_area(source_boxes, target_boxes)
+
+    assert area.shape == area_manual.shape, f"Expected same shape, but got area.shape = {area.shape}; area_manual.shape = {area_manual.shape}."
+    assert torch.equal(area, area_manual), f"Expected same value, but got area = {area}; area_manual = {area_manual}."
